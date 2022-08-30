16aa73c2c1b20679f44260538a100468cbb7b329
theonesomebodey <juniorflybear@gmail.com>
2022-08-29 18:00:47 -0800

    first version

---
+++ src/com/company/robot.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class robot {
+    void speak(String text) {
+        System.out.println(text);
+
+    }
+    public static void main(String[] args) {
+        robot r = new robot();
+        r.speak("bruh");
+        r.speak("bruh pt2");
+    }
+}
