eb175b0c43731a57b2b18d42bea26436dc9433f3
theonesomebodey <juniorflybear@gmail.com>
2022-08-29 18:29:58 -0800

    2nd

--- src/com/company/robot.java
+++ src/com/company/robot.java
@@ -4,8 +4,8 @@ public class robot {
     void speak(String text) {
         System.out.println(text);
     }
-    void name(String naam) {
-        System.out.println(naam);
+    void name(String name) {
+        System.out.println(name);
     }
     public static void main(String[] args) {
         robot r = new robot();
